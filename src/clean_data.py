107625178f1a51af71104331d9c5b830fd723ba5
malllikag <143150906+malllikag@users.noreply.github.com>
2023-12-04 17:36:11 -0800

    Update clean_data.py

--- src/clean_data.py
+++ src/clean_data.py
@@ -1,1 +1,17 @@
+import pandas as pd 
 
+def clean_data(dataframe):
+  dataframe = dataframe.dropna(axis=1, how='all')
+  dataframe = dataframe.dropna(subset=dataframe.columns[dataframe.isnull().any()])
+  return dataframe
+
+def save_cleaned_data(cleaned_dataframe, output_filename):
+  cleaned_dataframe.to_csv(output_filename, index=False)
+
+def clean_csv_file(input_filename, output_filename)
+  data=pd.read_csv(input_filename)
+  cleaned_data=clean_data(data)
+  save_cleaned_data(cleaned_data, output_filename)
+
+clean_csv_file('Regional_LACrimes_Yearly.csv', 'data/processed/cleaned_la_crimes.csv')
+clean_csv_file('ca_zip_pop.csv', 'data/processed/cleaned_ca_zip_pop.csv')
