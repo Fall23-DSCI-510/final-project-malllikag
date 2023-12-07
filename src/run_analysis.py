b84dc821f8c3cf928708f04ebf0ddf67339664e5
malllikag <143150906+malllikag@users.noreply.github.com>
2023-12-06 20:50:30 -0800

    Update run_analysis.py

--- src/run_analysis.py
+++ src/run_analysis.py
@@ -1,37 +1,42 @@
 import pandas as pd
-import plotly.express as px
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-df_6 = pd.read_csv('data/Crimes_6_LaRegions.csv')
+def analyze_crime_data(crime_data_file):
+    crime_df = pd.read_csv(data/processed/Crimes_6_LaRegions)
 
-df_6['Date_Rptd1'] = pd.to_datetime(df_6['Date_Rptd'], format='%m/%d/%Y %I:%M:%S %p')
+    # 1. Demographic Analysis: Bar chart representing distribution of victims by gender
+    plt.figure(figsize=(10, 6))
+    sns.countplot(x='Victim_Gender', data=crime_df, palette='pastel')
+    plt.title('Distribution of Victims by Gender')
+    plt.xlabel('Gender')
+    plt.ylabel('Count')
+    plt.savefig('results/victims_distribution_by_gender.png')
+    plt.show()
 
-df_6['Year'] = df_6['Date_Rptd1'].dt.year
-df_6['Month'] = df_6['Date_Rptd1'].dt.month
-df_6['Day'] = df_6['Date_Rptd1'].dt.day
+    # 2. Regional Analysis: Monthly crimes per region
+    region_monthly_crimes = crime_df.groupby(['Region', 'YearMonth'])['Crime_Type'].count().reset_index()
+    plt.figure(figsize=(12, 8))
+    sns.lineplot(x='YearMonth', y='Crime_Type', hue='Region', data=region_monthly_crimes)
+    plt.title('Monthly Crimes per Hotspot LA Region (2020-Present Day)')
+    plt.xlabel('Date')
+    plt.ylabel('Total Crimes')
+    plt.savefig('results/monthly_crimes_per_region.png')
+    plt.show()
 
-#1. Data Clock
-fig = px.sunburst(df_6, 
-                  path=['Year', 'Month', 'Day'], 
-                  title='Crime Frequency (2020-2023) Data Clock')
+    # 3. Anomaly Detection: Monthly crimes in Central region with 2 and 5 month moving averages
+    central_region_crimes = crime_df[crime_df['Region'] == 'Central']
+    central_region_crimes['Date'] = pd.to_datetime(central_region_crimes['Date'])
+    plt.figure(figsize=(12, 8))
+    sns.lineplot(x='Date', y='Crime_Type', data=central_region_crimes, label='Monthly Crimes')
+    sns.lineplot(x='Date', y='Crime_Type', data=central_region_crimes.rolling(window=8).mean(), label='2 Month Avg')
+    sns.lineplot(x='Date', y='Crime_Type', data=central_region_crimes.rolling(window=20).mean(), label='5 Month Avg')
+    plt.title('Monthly Crimes - Central Region')
+    plt.xlabel('Date')
+    plt.ylabel('Crime Count')
+    plt.legend()
+    plt.savefig('results/monthly_crimes_central_region.png')
+    plt.show()
 
-fig.write_html("LACrime_data_clock.html")
-
-#2. Hotspot map - created using ArcGIS Pro
-
-import arcpy
-
-crime_feature_class = 'Crimes_6_LaRegions.shp'
-
-crime_layer = arcpy.MakeFeatureLayer_management(crime_feature_class, 'crime_layer')
-
-hotspot_result = 'hotspot_result.shp'
-
-arcpy.stats.HotSpots('crime_layer', 'Crime_CD', hotspot_result)
-
-map_document = arcpy.mapping.MapDocument('CURRENT')
-data_frame = arcpy.mapping.ListDataFrames(map_document)[0]
-hotspot_layer = arcpy.mapping.Layer(hotspot_result)
-arcpy.mapping.AddLayer(data_frame, hotspot_layer)
-map_document.save()
-
-arcpy.mapping.ExportToPNG(map_document, 'hotspot_map.png', resolution=300)
+crime_data_file_path = 'data/processed/Crimes_6_LaRegions'
+analyze_crime_data(crime_data_file_path)
