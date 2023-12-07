3b1a03fbb13ca660d5762c8936d15146600b9a48
malllikag <143150906+malllikag@users.noreply.github.com>
2023-12-06 21:43:20 -0800

    Update visualize_results.py

--- src/visualize_results.py
+++ src/visualize_results.py
@@ -10,5 +10,20 @@ plt.title('Los Angeles Crime Frequency (2020-2023)')
 plt.show()
 
 # Visualization 2: Hotspot Analysis (created using ArcGIS Pro)
+![Severe Crime Hotspots](src/utils/hotspot.png)
 
 #Visualization 3: Fishnet Grid (created using ArcGIS Pro)
+![Fishnet Grid](src/utils/fishnet.png)
+
+#The following is code written in ArcPy in the ArcGIS Pro Python Notebook. 
+#1. Hotspot Analysis: 
+#import arcpy
+#input_shapefile = "Crimes_6_LaRegions.shp"
+#output_hotspot_map = "C:\\Users\\malli\\Documents\\Education\\M.S. Spatial Data Science\\Semester 2\\DSCI 510\\Screenshots\\Severe_Crime_Hotspots.png"
+#arcpy.analysis.HotSpots(input_shapefile, output_hotspot_map, "Part_1_2")
+
+#2. Fishnet Grid: 
+#import arcpy
+#input_shapefile = "Crimes_6_LaRegions.shp"
+#output_fishnet_grid = "C:\\Users\\malli\\Documents\\Education\\M.S. Spatial Data Science\\Semester 2\\DSCI 510\\Screenshots\\Fishnet_Grid.png"
+#arcpy.management.CreateFishnet(output_fishnet_grid, "xmin", "ymin", "xmax", "ymax", "cell_width", "cell_height", "number_rows", "number_columns", "labels", "geometry_type", "template", "build_polygon")
