6cd39c8ff4f256e06175898a3330c8661d892d62
malllikag <143150906+malllikag@users.noreply.github.com>
2023-12-08 15:32:23 -0800

    Update get_data.py

--- src/get_data.py
+++ src/get_data.py
@@ -1,47 +1,36 @@
 import pandas as pd 
 import numpy as np 
-import requests 
+import matplotlib.pyplot as plt 
+import seaborn as sns 
 
-def get_population_data():
-  pop_data = pd.read_csv('C:\Users\malli\Documents\Education\M.S. Spatial Data Science\Semester 3\SSCI 575\Final Project\LA_Crimes\CSV Files\ca_zip_pop.csv')
-  pop_data = pop_data.groupby('zipcode').agg({'population': 'max'}).reset_index()
-  ca_zips = pd.read_csv('C:\Users\malli\Documents\Education\M.S. Spatial Data Science\Semester 3\SSCI 575\Final Project\LA_Crimes\CSV Files\ca_zip_pop.csv'
-  pop_zip = pop_data.merge(ca_zips, how='inner', left_on='zipcode', right_on='ZIP_CODE')
-  return pop_zip
+df = pd.read_csv('superstore.csv')
 
-def get_lat_lng_coordinates(pop_zip):
-  lat, lng = [], []
+import requests
 
-  for idx in range(len(pop_zip)):
-    print(idx)
-    api_key = 'AIzaSyBziINbv_qWJy83YUIG4_cwRGvnFiCSHNg'
-    zip_code = pop_zip['ZIP_CODE'].iloc[idx]
-    api_url = f'https://maps.googleapis.com/maps/api/geocode/json?address={zip_code}&key={api_key}'
+def get_coordinates(api_key, city):
+    base_url = "https://maps.googleapis.com/maps/api/geocode/json"
+    params = {
+        'address': city,
+        'key': api_key
+    }
 
-    response = requests.get(api_url)
-    print(response)
+    response = requests.get(base_url, params=params)
     data = response.json()
 
-    #Extract coordinates
-    try: 
-      coords = data['results'][0]['geometry']['location']
-      lat.append(coords['lat'])
-      lng.append(coords['lng'])
-    except:
-      lat.append(np.nan)
-      lng.append(np.nan)
+    if data['status'] == 'OK':
+        location = data['results'][0]['geometry']['location']
+        latitude = location['lat']
+        longitude = location['lng']
+        return latitude, longitude
+    else:
+        return None
 
-  return lat, lng
+api_key = 'AIzaSyDWK90QN3AbqrwyI3UvFhToYkmHz7jJlPk'
+cities = ['Los Angeles', 'San Francisco', 'New York City', 'Philadelphia', 'Santo Domingo']
+for city in cities:
+    coordinates = get_coordinates(api_key, city)
+    if coordinates:
+        print(f"Coordinates for {city}: {coordinates}")
+    else:
+        print(f"Unable to retrieve coordinates for {city}")
 
-def main():
-  population_data = get_population_data()
-  latitudes, longitudes = get_lat_lng_coordinates(population_data)
-
-  population_data['latitude'] = latitudes
-  population_data['longitude'] = longitudes
-
-  population_data.to_csv('data/raw/population_data_with_coordinates.csv', index=False)
-
-if __name__=="__main__":
-  main()
-    
