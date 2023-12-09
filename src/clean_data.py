8b032995e1bd952c22396ab88ed1c8f089434e77
malllikag <143150906+malllikag@users.noreply.github.com>
2023-12-08 19:40:04 -0800

    Update clean_data.py

--- src/clean_data.py
+++ src/clean_data.py
@@ -1,1 +1,26 @@
-Please refer to .ipynb
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt 
+import seaborn as sns 
+
+# Display basic information about the dataset
+print("Original DataFrame:")
+print(df.info())
+
+# Handling Missing Values
+df.dropna(inplace=True)
+
+# Removing Duplicates
+df.drop_duplicates(inplace=True)
+
+# Convert Date column to datetime format
+df['Order.Date'] = pd.to_datetime(df['Order.Date'])
+
+# Extract Year and Month from the OrderDate
+df['Year'] = df['Order.Date'].dt.year
+df['Month'] = df['Order.Date'].dt.month
+
+print("\nCleaned DataFrame:")
+print(df.head())
+
+df.to_csv('cleaned_superstore.csv', index=False)
